2d59db769607efac73d506d21b150f0ec3d9358f
pravakar <pravakark46@gmail.com>
2024-11-23 23:48:27 +0530

    hello world print

--- app/src/main/java/com/example/androidappdevelopment/MainActivity.kt
+++ app/src/main/java/com/example/androidappdevelopment/MainActivity.kt
@@ -18,30 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AndroidAppDevelopmentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+           Text(text = "Hello World!")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidAppDevelopmentTheme {
-        Greeting("Android")
     }
 }
